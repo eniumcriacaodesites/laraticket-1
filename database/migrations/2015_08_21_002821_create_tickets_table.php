e13829428ce7f8ec3918a4166cc015f4f9b478d7
Jeff Kilroy <jeffkilroy@gmail.com>
2015-08-20 20:52:39 -0400

    Create base migrations

---
+++ database/migrations/2015_08_21_002821_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('client_id');
+            $table->integer('status_id');
+            $table->integer('priority');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tickets');
+    }
+}
