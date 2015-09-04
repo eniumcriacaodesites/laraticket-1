5982e1beddb7704d946255fad3eb46cdf1ecdc52
Jeff Kilroy <jeffkilroy@gmail.com>
2015-09-04 17:03:18 -0400

    Set priority colors

--- resources/views/ticket/partials/panel.blade.php
+++ resources/views/ticket/partials/panel.blade.php
@@ -1,13 +1,15 @@
-<div class="panel panel-default panel-ticket">
-    <div class="panel-heading">
+<div class="panel panel-default panel-ticket ticket-priority-{{ $ticket->priority }}">
+    <div class="panel-heading" data-toggle="collapse" data-target="#collapse-ticket-panel-{{ $ticket->id }}">
         <h3 class="panel-title">
-                @if($ticket->client)
-                    <span class="label label-info">{{ $ticket->client->name }}</span>
-                @endif
-                    {{ $ticket->title }}
+            @if($ticket->client)
+                <span class="label label-info">{{ $ticket->client->name }}</span>
+            @endif
+            {{ $ticket->title }}
         </h3>
     </div>
-    <div class="panel-body">
-        <div class="description">{{ $ticket->description }}</div>
+    <div id="collapse-ticket-panel-{{ $ticket->id }}" class="panel-collapse collapse out">
+        <div class="panel-body">
+            <div class="description">{{ $ticket->description }}</div>
+        </div>
     </div>
 </div>
